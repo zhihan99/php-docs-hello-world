af3cb3e0ef159bffdccfd02be6982f36b52bb4b0
zhihan99 <76796868+zhihan99@users.noreply.github.com>
2022-06-27 22:39:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,78 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+<title>Merdu Music CD Shop</title>
+<link rel="stylesheet" href="Lab9CDShop\LabCSS\mystyle.css"> 
+</head>
+<header>
+    <h1><img src="LabCSS\musicicon.png" style="width: 30px; height:30px;">
+    Merdu Music CD Shop
+    <a href="#cart" style="float: right; text-decoration: none;">Shopping Cart</a>
+    <img src="https://i.pinimg.com/originals/15/4f/df/154fdf2f2759676a96e9aed653082276.png" style="width: 30px; height:30px; float: right;" >
+    </h1>
+    <div class="topnav">
+        <a class="active" href="Lab9CDShop\CDShop.php">Home</a>
+        <a href="Lab9CDShop\CDList.php">CDs</a>
+        <a href="#events">Events</a>
+        <a href="#about">About</a>
+        <a href="Lab9CDShop\registration.php">Register</a>
+    </div>
+</header>
+<body>
+    <div class="sidenav">
+        <b>Categories</b><br>
+        <a href="Lab9CDShop\kpop.php">K-pop</a>
+        <a href="Lab9CDShop\jpop.php">J-pop</a>
+        <a href="Lab9CDShop\anime.php">Anime</a>
+        <a href="Lab9CDShop\soundtrack.php">Soundtrack</a>
+        <a href="Lab9CDShop\western.php">Western Music</a>
+      </div>
+    <br>
+    <h2 style="border-bottom: solid; ">TopSellers</h2>
+    <table style="width: 100%; table-layout: fixed;" >
+        <tr>
+            <td>
+                <br>
+                <img src="https://cf.shopee.com.my/file/093cb6cb3481549aa4329514c27d5bd7" style="width: 300px; height:300px;" ><br>
+                <a href="Lab9CDShop\VBS.php">Vivid Bad Squad - Ready Steady / Forward</a>
+            </td>
+            <td>
+                <img src="https://upload.wikimedia.org/wikipedia/en/2/21/BTS_-_Map_of_the_Soul_7.png" style="width: 300px; height:300px;" ><br>
+                <a href="Lab9CDShop\BTS.php">BTS - Map of the Soul: 7</a>
+            </td>
+            <td>
+                <img src="https://static.wikia.nocookie.net/ladygaga/images/7/75/TheFame.jpg/revision/latest/scale-to-width-down/2000?cb=20110410191050" style="width: 300px; height:300px;" ><br>
+                <a href="Lab9CDShop\LadyGaga.php">The Fame - Lady Gaga</a>
+            </td>
+        </tr>
+    </table>
+    <br>
+    <h2 style="border-bottom: solid; ">New Releases</h2>
+    <table style="width: 100%; table-layout: fixed;">
+        <tr>
+            <td>
+                <img src="https://upload.wikimedia.org/wikipedia/en/9/90/Twice_-_Eyes_Wide_Open.png" style="width: 300px; height:300px;" ><br>
+                <a href="Lab9CDShop\Twice.php">EYES WIDE OPEN - TWICE 2nd Album </a>
+            </td>
+            <td>
+                <br>
+                <img src="https://static.wikia.nocookie.net/finalfantasy/images/a/ab/Final_Fantasy_VII_Remake_Original_Soundtrack_Cover.jpg/revision/latest?cb=20200527013722" style="width: 300px; height:300px;" ><br>
+                <a href="Lab9CDShop\FF7.php">Final Fantasy VII Remake Original Soundtrack</a>
+            </td>
+            <td>
+                <img src="https://m.media-amazon.com/images/I/71nRHTIxYfL._SL1044_.jpg" style="width: 300px; height:300px;" ><br>
+                <a href="Lab9CDShop\Spyair.php">Million - Spyair</a>
+            </td>
+        </tr>
+    </table>
+</body>
+<footer>
+    <ul>
+            <li><a href="CDShop.html">Home</a></li>
+            <li><a href="CDList">CDs</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="registration.html">Register</a></li>
+    </ul>
+</footer>
+</html>
